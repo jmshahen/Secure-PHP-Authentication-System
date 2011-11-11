20b6ef327bade3ca438ffc48747803f8a9f12bb1
Pongs <pongs1@live.com>
2011-11-10 19:21:58 -0500

    Need to Implement: -> secure_user_auth::trying_to_login() -> secure_user_auth::security_test_credentials() -> secure_user_auth::security_blocked_ip(); -> secure_user_auth::display_login_script()

--- login.php
+++ login.php
@@ -8,4 +8,40 @@
 * @version 1.0
 * @package login
 */
+
+include_once "scripts/secure_user_auth.inc"
+
+$trying_to_login = secure_user_auth::trying_to_login();
+
+if($trying_to_login === 1)
+{
+  //check the user's credentials
+	if(secure_user_auth::security_test_credentials() === 1)
+	{
+		//redirect the user to the default page or to a redirect page if provided
+		
+	}
+	else
+	{
+		//display invalid login (indescriptive)
+	}
+}
+else if($trying_to_login === -1)
+{
+	//display message for blocked IP addresses
+	secure_user_auth::security_blocked_ip();
+}
+else if($trying_to_login === 0)
+{
+	//display login script
+	secure_user_auth::display_login_script();
+}
+else
+{
+	//Should never run this else statement, but needed for programmer's mistakes
+	print "Whoops, this shouldn't be appearing.<br/>".
+	"Error Code: <b>$trying_to_login</b>";
+	
+	exit(0);
+}
 ?>
